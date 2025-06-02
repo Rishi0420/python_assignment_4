10bedfed3da65297278ca186ef43fd17280d81df
Rushikesh Phatak <rushikeshsphatak04@gmail.com>
2025-06-02 18:31:43 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,11 @@
+# Task 1: Read a File and Handle Errors
+try:
+    with open("sample.txt", "r") as f:
+        print("Reading file content:")
+        c = 1
+        for line in f:
+            print("Lines", c, ":", line.strip())
+            # print("Lines",c,":",line)
+            c += 1
+except FileNotFoundError:
+    print("Error: The file 'sample.txt' was not found.")
